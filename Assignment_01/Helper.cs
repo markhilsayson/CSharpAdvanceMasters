4dab548e9e152fbad53c9c5869af2423fec9cf32
markhilsayson <markhilsayson@gmail.com>
2023-02-17 20:48:15 +0800

    Session 1 & 2 Assignment (#1)

---
+++ Assignment_01/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_01
+{
+    public static class Helper
+    {
+        public static bool IsValidSelection(this string num) { 
+            return num=="1" || num=="2";
+        }
+
+        public static bool IsValidYesOrNo(this string num)
+        {
+            return num.ToLower() == "y" || num.ToLower() == "n";
+        }
+    }
+}
---
+++ Assignment_01/Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_01
+{
+    public class Video
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Year { get; set; }
+        public bool IsAvailable { get; set; } = true;
+    }
+}

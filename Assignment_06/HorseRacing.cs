3cf53eb8556c6ad84855222ee4b4713e2cace183
markhilsayson@gmail.com <markhilsayson@gmail.com>
2023-03-28 05:11:54 +0800

    Add progressBar

--- Assignment_06/HorseRacing.cs
+++ Assignment_06/HorseRacing.cs
@@ -8,8 +8,9 @@ namespace Assignment_06
 {
     internal class HorseRacing
     {
-        public void Run() {
-            
+        public void Run()
+        {
+
             var runners = new List<Horse>();
             for (int i = 1; i <= 5; i++)
             {
@@ -20,7 +21,7 @@ namespace Assignment_06
 
             Console.WriteLine("Welcome to Horse Racing Game!");
             Console.WriteLine("Please place your bet for one of the horses (1-5):");
-            
+
             int bet;
 
             while (!int.TryParse(Console.ReadLine(), out bet) || bet < 1 || bet > 5)
@@ -30,53 +31,38 @@ namespace Assignment_06
 
             Console.WriteLine("Starting the race...");
             var raceFinishedEvent = new ManualResetEvent(false);
-       
+            int totalWork = 100;
+            int completedWork = 0;
+
             foreach (var runner in runners)
             {
+
+
                 new Thread(() =>
                 {
-                    Console.WriteLine($"{Thread.CurrentThread.Name} is starting to run.");
-                    while (!raceFinishedEvent.WaitOne(0))
+                    while (completedWork < totalWork)
                     {
-                        runner.Move();
+                        Console.WriteLine();
+                        completedWork++;
+
+                        Console.Write("\r" + Thread.CurrentThread.Name.ToString() + ": [{0}{1}] {2}%", new string('#', completedWork), new string('-', totalWork - completedWork), completedWork);
+
+                        if (completedWork == totalWork)
+                        {
+                            Console.WriteLine("\nWinner: " + Thread.CurrentThread.Name.ToString());
+                        }
+                        Thread.Sleep(100);
                     }
+
                 })
                 {
+                    /*Thread Name*/
                     Name = runner.Name,
+
+                    /*Thread Priority*/
                     Priority = runner.Speed >= 4 ? ThreadPriority.Highest : (ThreadPriority)runner.Speed
                 }.Start();
             }
-
-            raceFinishedEvent.Set();
-
-            Console.WriteLine("Results:");
-            runners.Sort((h1, h2) => h2.Position.CompareTo(h1.Position));
-
-            int ctr = 1;
-            foreach (var runner in runners)
-            {
-                string awardPosition = string.Empty;
-                switch (ctr) {
-                    case 1: awardPosition = "1st"; break;
-                    case 2: awardPosition = "2nd"; break;
-                    case 3: awardPosition = "3rd"; break;
-                    case 4: awardPosition = "4th"; break;
-                    case 5: awardPosition = "5th"; break;
-                }
-                Console.WriteLine($"{runner.Name} {awardPosition} winner.");
-                ctr++;
-            }
-
-            if (runners[bet - 1].Position == runners[0].Position)
-            {
-                Console.WriteLine($"Congratulations! {runners[bet - 1].Name} won the race.");
-            }
-            else
-            {
-                Console.WriteLine($"Sorry, {runners[bet - 1].Name} did not win the race.");
-            }
-
-            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
 
@@ -85,5 +71,7 @@ namespace Assignment_06
             var random = new Random();
             return random.Next(1, 6);
         }
+
     }
+
 }
